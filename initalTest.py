389a98ceee5809ba42d70ba060e7f55b4f6e6fab
pbrandebura <p.brandebura@gmail.com>
2018-06-03 17:23:41 +0200

    Logging as admin

--- initalTest.py
+++ initalTest.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def test():
-    url = "http://www.google.com/"
+    url = "http://localhost/litecart/admin/"
     wd = webdriver.Firefox()
     wd.get(url)
-    wd.implicitly_wait(5)
+    WebDriverWait(wd, 10).until(EC.presence_of_element_located((By.ID, 'box-login')))
+    wd.find_element(By.NAME, 'username').send_keys("admin")
+    wd.find_element(By.NAME, 'password').send_keys("admin")
+    wd.find_element(By.NAME, 'login').click()
     wd.quit()
