b846c1e1da543fd5ac2c88a0aca1566b2c6de30f
Patryk Brandebura <p.brandebura@gmail.com>
2020-03-31 20:19:14 +0200

    Added constructor and assertion for objects in product details tests

--- tests/test_storefront_litecart.py
+++ tests/test_storefront_litecart.py
@@ -8,4 +8,7 @@ def test_verify_number_of_sale_label(app):
 
 def test_confirm_pdp_opens_with_correct_details(app):
     app.store_front.navigate_to_litecart_shop()
-    app.store_front.select_first_product_in_campaigns()
+    product_details_front = app.store_front.get_first_campaign_product_details_store_front()
+    app.store_front.open_pdp_for_first_product()
+    product_details_pdp = app.store_front.get_product_details_from_pdp()
+    assert product_details_front==product_details_pdp
