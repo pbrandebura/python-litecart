c965d59fbbeeb5a27676d85635398a8cf12ca92d
Patryk Brandebura <p.brandebura@gmail.com>
2020-04-09 13:24:23 +0200

    Add/remove products finished

--- fixture/basket.py
+++ fixture/basket.py
@@ -1,4 +1,3 @@
-from time import sleep
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
@@ -13,53 +12,47 @@ class Basket:
 
     def add_product_from_most_popular(self):
         driver = self.app.driver
-        self.add_product_to_basket("box-most-popular")
-        sleep(5)
-        self.basket_status_check(1)
+        self.add_product_to_basket("box-most-popular", '1')
         self.go_to_homepage()
 
     def add_product_from_campaigns(self):
         driver = self.app.driver
-        self.add_product_to_basket("box-campaigns")
-        sleep(5)
-        self.basket_status_check(2)
+        self.add_product_to_basket("box-campaigns", '2')
         self.go_to_homepage()
 
     def add_product_from_latest_products(self):
         driver = self.app.driver
-        self.add_product_to_basket("box-latest-products")
-        sleep(5)
-        self.basket_status_check(3)
-        self.go_to_homepage()
+        self.add_product_to_basket("box-latest-products", '3')
 
-    def basket_status_check(self, expected_value):
-        driver = self.app.driver
-        basket_status = driver.find_element_by_css_selector('span.quantity').text
-        print("Hey to jest basket status:" + basket_status)
-        assert int(basket_status) == expected_value
-
-    def go_to_homepage(self):
-        driver = self.app.driver
-        driver.find_element_by_css_selector('#logotype-wrapper').click()
-
-    def add_product_to_basket(self, product_list):
+    def add_product_to_basket(self, product_list, size):
         driver = self.app.driver
         product = driver.find_element_by_xpath('//*[@id="' + product_list + '"]/div/ul/li[1]').click()
-        self.choose_item_size()
-        sleep(2)
-        driver.find_element_by_name('add_cart_product').click()
+        basket_qty_before_adding = driver.find_element_by_css_selector('span.quantity').text
+        self.choose_item_size(size)
+        add_to_cart_btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "add_cart_product")))
+        add_to_cart_btn.click()
+        self.basket_status_check(basket_qty_before_adding)
 
-    def choose_item_size(self):
+    def basket_status_check(self, actual_basket_status):
         driver = self.app.driver
+        expected_value = 1 + int(actual_basket_status)
+        WebDriverWait(driver, 10).until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, 'span.quantity'), str(expected_value)))
 
+    def choose_item_size(self, size):
+        driver = self.app.driver
         try:
             driver.implicitly_wait(1)
             driver.find_element_by_css_selector('td.options')
             s1 = Select(driver.find_element_by_name('options[Size]'))
             for opt in s1.options:
-                s1.select_by_index('1')
+                s1.select_by_index(size)
         except NoSuchElementException:
             pass
+
+    def go_to_homepage(self):
+        driver = self.app.driver
+        driver.find_element_by_css_selector('#logotype-wrapper').click()
 
     def open_basket(self):
         driver = self.app.driver
@@ -67,6 +60,13 @@ class Basket:
 
     def clear_basket(self):
         driver = self.app.driver
-        wait = WebDriverWait(driver, 5)
-        remove_button = wait.until(EC.presence_of_element_located(By.NAME("remove_cart_item")))
-        remove_button.click()
+        expected_value = "Customer Details"
+        WebDriverWait(driver, 10).until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#box-checkout-customer > h2'), expected_value))
+        items_in_basket = driver.find_elements_by_css_selector('td.sku')
+        for element in range(0, len(items_in_basket)+1):
+            remove_button = WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "remove_cart_item")))
+            remove_button.click()
+        empty_cart_info = "There are no items in your cart."
+        WebDriverWait(driver, 10).until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#checkout-cart-wrapper > p:nth-child(1) > em'), empty_cart_info))
